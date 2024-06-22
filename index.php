3239d29ac144dd41e56757729d92f8f94c690ba7
csarq <catherinesarquis@gmail.com>
2024-06-22 12:14:12 +0100

    HTML structure with placeholders

--- index.php
+++ index.php
@@ -7,7 +7,22 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+  <header>   
     <h1>🌎 Country Fact Generator 🌎</h1>
+  </header>
+
+  <main>
+    <section aria-labelledby="countryName" role="region">
+            <h2 id="countryName">Country Name</h2>
+            <img id="countryFlag" src="placeholder.jpg" alt="Country Flag Description To Go Here">
+            <p>Region: <span id="countryRegion">Placeholder</span></p>
+            <p>Population: <span id="countryPopulation">Placeholder</span></p>
+            <p>Languages: <span id="countryLanguages">Placeholder</span></p>
+            <p>Capital: <span id="countryCapital">Placeholder</span></p>
+            <p>Currency: <span id="countryCurrency">Placeholder</span></p>
+    </section>
+    <button id="newCountryBtn">New country</button>
+</main>
     
     <?php
        //do stuff here
