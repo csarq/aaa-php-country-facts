b7a39797ce3e0e1307e69349fe7641e4eabe3a06
csarq <catherinesarquis@gmail.com>
2024-06-22 15:45:45 +0100

    display all country info, link button to refresh page and some styling

--- index.php
+++ index.php
@@ -16,20 +16,104 @@
       //print the random country to console
       echo "<script>console.log('Random Country: $randomCountry');</script>";
 
+      
+
+      //$encodedCountryName = urlencode($randomCountry); didn't work!
+
       //fetch data for the random country
       $countryDataURL = "https://restcountries.com/v3.1/name/" . $randomCountry;
+
+      // Check if the URL contains a space
+if (strpos($countryDataURL, ' ') !== false) {
+    // If it does, replace spaces with %20
+    $countryDataURL = str_replace(' ', '%20', $countryDataURL);
+}
+
+      /*     
+      testing if the URL contains a space
+
+      $pattern = '/\s/'; //pattern to match white spaces
+
+      $matchResult = preg_match($pattern, $countryDataURL);
+
+echo "<script>";
+if ($matchResult) {
+    echo "console.log('URL contains a space!');";
+} else {
+    echo "console.log('URL does not contain a space.');";
+}
+echo "</script>";*/
+
       $GetCountryData = file_get_contents($countryDataURL);
       $countryData = json_decode($GetCountryData, true);
 
-      var_dump($countryData);
+      //var_dump($countryData);
 
       //print the country data to console to check it's correct
       echo "<script>console.log('Country Data: " . json_encode($countryData) . "');</script>";
 
 $countryFlagImage = $countryData[0]["flags"]["svg"]; 
-$countryFlagAlt = $countryData[0]["flags"]["alt"];
+// if flag is missing Alt text, then display "Image of the flag of country"
+$countryFlagAlt = $countryData[0]["flags"]["alt"] ?? "Image of the flag of " . $randomCountry; 
+
+$region = $countryData[0]["region"];
+$population = $countryData[0]["population"];
+
+//$languages = $countryData[0]["languages"];
+//var_dump($languages); 
+
+//loop through the languages array and extract the names of the languages
+$languages = array();
+foreach($countryData[0]["languages"] as $language){
+  $languages[] = $language; //add the language to the languages array
+}
+
+//change the languages array to a string
+$languages = implode(", ", $languages); // implode(string $separator, array $array): string
+
+$capital = $countryData[0]["capital"][0];
+
+$currencies = array();
+foreach($countryData[0]["currencies"] as $currency){
+  $currencies[] = $currency["name"]; //add the currency to the currencies array
+} 
+
+$currency = implode(", ", $currencies); 
+
+/*to fix:
+//☑️ fixed 1. when a country has spaces in the name, the fetch request fails, but https://restcountries.com/v3.1/name/Western%20Sahara is correct, for example.
+// ☑️ Fixed 2. sometimes there is not any Alt text for a flag - apply a default Alt text using the country name variable
+//it doesn't like https://restcountries.com/v3.1/name/Saint%20Barthélemy in borwser it's fine, but not in fetch request could be the accent
+3. https://restcountries.com/v3.1/name/São%20Tomé%20and%20Príncipe - issue with fetching but fine in browser
+4. issue with Bouvet Island: 
+Warning
+: Undefined array key "capital" in
+C:\xampp\htdocs\aaa-php-country-facts\index.php
+on line
+74
 
 
+Warning
+: Trying to access array offset on value of type null in
+C:\xampp\htdocs\aaa-php-country-facts\index.php
+on line
+74
+
+
+Warning
+: Undefined array key "currencies" in
+C:\xampp\htdocs\aaa-php-country-facts\index.php
+on line
+77
+
+
+Warning
+: foreach() argument must be of type array|object, null given in
+C:\xampp\htdocs\aaa-php-country-facts\index.php
+on line
+77
+
+*/
 
     ?>
 
@@ -48,13 +132,15 @@ $countryFlagAlt = $countryData[0]["flags"]["alt"];
     <section aria-labelledby="countryName" role="region">
             <h2 id="countryName"><?= $randomCountry?></h2>
             <img id="countryFlag" src="<?= $countryFlagImage;?>" alt="<?=$countryFlagAlt; ?>"/>
-            <p>Region: <span id="countryRegion">Placeholder</span></p>
-            <p>Population: <span id="countryPopulation">Placeholder</span></p>
-            <p>Languages: <span id="countryLanguages">Placeholder</span></p>
-            <p>Capital: <span id="countryCapital">Placeholder</span></p>
-            <p>Currency: <span id="countryCurrency">Placeholder</span></p>
+            <p>Region: <span id="countryRegion"><?= $region;?></span></p>
+            <p>Population: <span id="countryPopulation"><?= $population;?></span></p>
+            <p>Languages: <span id="countryLanguages"><?= $languages;?></span></p>
+            <p>Capital: <span id="countryCapital"><?= $capital;?></span></p>
+            <p>Currency: <span id="countryCurrency"><?= $currency;?></span></p>
+
     </section>
-    <button id="newCountryBtn">New country</button>
+                <button id="newCountryBtn">New country</button>
+     </main> 
 </main>
     
   
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Script.js is loaded!");
+document.addEventListener("DOMContentLoaded", function () {
+  const newCountryBtn = document.getElementById("newCountryBtn");
+
+  newCountryBtn.addEventListener("click", function () {
+    // Initiate a new PHP request to generate a fresh random country
+    window.location.href = "index.php"; // This reloads the page with new data
+  });
+});
+
+// This reloads the page with new data without AJAX, although it causes repetition for the same country and screenreaders in repeating the heading.
